f2ed53447f07c8b070611cbd5a5451e9967cb242
Tsvetan Dimoff <tsdimo@gmail.com>
2024-01-26 16:51:48 +0200

    Restrict status and investment types

--- src/schemas/investment.schema.ts
+++ src/schemas/investment.schema.ts
@@ -1,9 +1,18 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object().shape({
-  type: yup.string().required("Type is required"),
+  type: yup
+    .string()
+    .required("Type is required")
+    .oneOf(
+      ["fixed-income", "stock", "commodity", "crypto"],
+      "Type must be fixed-income, stock, commodity or crypto"
+    ),
   value: yup.number().required("Value is required"),
   date: yup.string().required("Date is required"),
-  status: yup.string().required("Status is required"),
+  status: yup
+    .string()
+    .required("Status is required")
+    .oneOf(["active", "closed"], "Status can only be active or closed"),
   name: yup.string().required("Name is required"),
 });
